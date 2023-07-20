f8baaa0e6a42970d117930f07a74304959ccc4ed
Rudra Vaibhav <rudravaibhav45@gmail.com>
2023-07-20 19:58:49 +0530

    modified code and simulation result

--- PWM_Generator.srcs/sim_1/new/pwm_generator_tb.v
+++ PWM_Generator.srcs/sim_1/new/pwm_generator_tb.v
@@ -19,75 +19,126 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module pwm_generator_tb;
 
-  reg clk;
-  reg reset;
-  reg enable;
-  reg [7:0] duty_cycle;
-  reg duty_inc;
-  reg duty_dec;
-  reg [15:0] frequency;
-  wire pwm_out;
+    // Inputs
+    reg clk;
+    reg increase_duty;
+    reg decrease_duty;
 
-  // Clock generation
-  always begin
-    #5 clk = ~clk;
-  end
+    // Outputs
+    wire PWM_OUT;
+    wire [3:0] DUTY_CYCLE_OUT;
+    wire INC_DUTY;
+    wire DEC_DUTY;
 
-  initial begin
-    clk = 0;
-    reset = 0;
-    enable = 1;
-    duty_cycle = 8'b01010101;  // Initial duty cycle value
-    duty_inc = 0;
-    duty_dec = 0;
-    frequency = 16'b000110100100;  // Initial frequency value
+    // Instantiate the PWM Generator with variable duty cycle in Verilog
+    pwm_generator DUT (
+        .clk(clk),
+        .increase_duty(increase_duty),
+        .decrease_duty(decrease_duty),
+        .PWM_OUT(PWM_OUT),
+        .DUTY_CYCLE_OUT(DUTY_CYCLE_OUT),
+        .INC_DUTY(INC_DUTY),
+        .DEC_DUTY(DEC_DUTY)
+    );
 
-    // Apply reset
-    reset = 1;
-    #10 reset = 0;
+    // Create 100Mhz clock
+    initial begin
+        clk = 0;
+        forever #5 clk = ~clk;
+    end
 
-    // Display initial state
-    $display("Time = %0t | Duty Cycle = %b | Frequency = %d | PWM Output = %b", $time, duty_cycle, frequency, pwm_out);
+    initial begin
+        // Initial values for inputs
+        increase_duty = 0;
+        decrease_duty = 0;
 
-    // Wait for a few cycles
-    #30;
+        // Test all combinations of inputs
 
-    // Increase duty cycle
-    duty_inc = 1;
-    #20;
-    duty_inc = 0;
-    #20;
-    
-    // Decrease duty cycle
-    duty_dec = 1;
-    #20;
-    duty_dec = 0;
-    #20;
+        // 1. Increment duty cycle once
+        #100;
+        increase_duty = 1;
+        #100;
+        increase_duty = 0;
 
-    // Change frequency
-    frequency = 16'b111111111111;
-    #20;
+        // 2. Decrement duty cycle once
+        #100;
+        decrease_duty = 1;
+        #100;
+        decrease_duty = 0;
 
-    // Finish simulation
-    #20 $finish;
-  end
+        // 3. Increment duty cycle multiple times
+        repeat (5) begin
+            #100;
+            increase_duty = 1;
+            #100;
+            increase_duty = 0;
+        end
 
-  // Instantiate PWM generator module
-  pwm_generator pwm_gen (
-    .clk(clk),
-    .reset(reset),
-    .enable(enable),
-    .duty_cycle(duty_cycle),
-    .duty_inc(duty_inc),
-    .duty_dec(duty_dec),
-    .frequency(frequency),
-    .pwm_out(pwm_out)
-  );
+        // 4. Decrement duty cycle multiple times
+        repeat (5) begin
+            #100;
+            decrease_duty = 1;
+            #100;
+            decrease_duty = 0;
+        end
+
+        // 5. Increment and decrement duty cycle multiple times
+        repeat (5) begin
+            #100;
+            increase_duty = 1;
+            #100;
+            increase_duty = 0;
+            #100;
+            decrease_duty = 1;
+            #100;
+            decrease_duty = 0;
+        end
+
+        // 6. Increment and decrement duty cycle together (cancel each other)
+        repeat (5) begin
+            #100;
+            increase_duty = 1;
+            decrease_duty = 1;
+            #100;
+            increase_duty = 0;
+            decrease_duty = 0;
+        end
+
+        // 7. No change in duty cycle
+        #500;
+
+        // 8. Random combination of inputs
+        #100;
+        increase_duty = 1;
+        #100;
+        decrease_duty = 1;
+        #100;
+        increase_duty = 0;
+        decrease_duty = 0;
+
+        // 9. Additional duty cycle decrements
+        #100;
+        decrease_duty = 1;
+        #100;
+        decrease_duty = 0;
+        #100;
+        decrease_duty = 1;
+        #100;
+        decrease_duty = 0;
+        #100;
+        decrease_duty = 1;
+        #100;
+        decrease_duty = 0;
+
+        // End of simulation
+        #100;
+        $finish;
+    end
 
 endmodule
+
 
 
 
